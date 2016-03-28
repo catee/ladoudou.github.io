21e0c4766c0cc57154b9d5994bafab701fad2ad9
catee <452416115@qq.com>
2016-03-28 22:13:44 +0800

    二维数组
    rt

--- ladoudou/mine/js/app.js
+++ ladoudou/mine/js/app.js
@@ -3,9 +3,11 @@ requirejs.config({
 });
 
 // Start the main app logic.
-requirejs(['test', 'render/render'], function (a, Render) {
+requirejs(['test', 'render/render', 'data/DyadicArray'], function (a, Render, DyadicArray) {
     console.log(a);
     window.R = new Render({
         canvas: document.querySelector('#canvas')
     });
+    window.D = new DyadicArray([1,0,0,1,1,1,0,0,1,0,0,1], 3, 4);
+    window.C = new DyadicArray(new Float32Array([1, 1, 1, 1, 0, 1, 1, 1, 1]), 3, 3);
 });
--- ladoudou/mine/js/modules/data/DyadicArray.js
+++ ladoudou/mine/js/modules/data/DyadicArray.js
@@ -4,7 +4,7 @@ define(['utils/typeChecker'], function (typeChecker) {
 
     var DyadicArray = function (array, m, n) {
 
-        if (getType(array) !== 'array') {
+        if (getType(array) !== 'array' && getType(array) !== 'float32array') {
             throw Error('源数据必须是数组');
         }
 
@@ -13,8 +13,8 @@ define(['utils/typeChecker'], function (typeChecker) {
         }
 
         this.source = array;
-        this.row = n;
-        this.colum = m;
+        this.row = m;
+        this.colum = n;
 
     };
 
@@ -24,21 +24,21 @@ define(['utils/typeChecker'], function (typeChecker) {
 
         getValue: function (x, y) {
 
-            if (x < 0 || x > (this.colum - 1) || y < 0 || y > (this.row - 1)) {
+            if (x < 0 || x > (this.row - 1) || y < 0 || y > (this.colum - 1)) {
                 throw Error('坐标超出矩阵边界');
             }
 
-            return this.source[x + y * this.colum];
+            return this.source[y + x * this.colum];
 
         },
 
         setValue: function (x, y, newValue) {
 
-            if (x < 0 || x > (this.colum - 1) || y < 0 || y > (this.row - 1)) {
+            if (x < 0 || x > (this.row - 1) || y < 0 || y > (this.colum - 1)) {
                 throw Error('坐标超出矩阵边界');
             }
 
-            var index = x + y * this.colum;
+            var index = y + x * this.colum;
             if (getType(this.source[index]) !== getType(newValue)) {
                 throw Error('新值与原值类型不统一');
             }
@@ -53,7 +53,7 @@ define(['utils/typeChecker'], function (typeChecker) {
                 throw Error('卷积核必须是 DyadicArray 类型');
             }
 
-            if (/^float(32)|(64)array$/i.test(getType(core.source))) {
+            if (! /^float32array$/i.test(getType(core.source))) {
                 throw Error('卷积核必须是类型化数组 Float32Array');
             }
 
@@ -83,10 +83,40 @@ define(['utils/typeChecker'], function (typeChecker) {
             var row = this.row;
             var colum = this.colum;
             var coreSize = core.row;
+            var halfCoreSize = (coreSize - 1) / 2;
             var rowZeroed = row + coreSize - 1;
             var columZeroed = colum + coreSize - 1;
             var lengthZeroed = rowZeroed * columZeroed;
             var sourceZeroed = new Float32Array(lengthZeroed);
+            var thisZeroed = new DyadicArray(sourceZeroed, rowZeroed, columZeroed);
+            var result = new DyadicArray(new Float32Array(row * colum), row, colum);
+
+            for (var i = 0; i < rowZeroed; i ++) {
+                for (var j = 0; j < columZeroed; j ++) {
+                    if (i < halfCoreSize || i >= halfCoreSize + row || j < halfCoreSize || j >= halfCoreSize + colum) {
+                        thisZeroed.setValue(i, j, 0.0);
+                    } else {
+                        var v = this.getValue(i - halfCoreSize, j - halfCoreSize);
+                        thisZeroed.setValue(i, j, parseFloat(v));
+                    }
+                }
+            }
+
+            console.log(thisZeroed);
+
+            for (var i = halfCoreSize; i < row + halfCoreSize; i ++) {
+                for (var j = halfCoreSize; j < colum + halfCoreSize; j ++) {
+                    var _result = 0;
+                    for (var m = 0; m < coreSize; m ++) {
+                        for (var n = 0; n < coreSize; n ++) {
+                            _result += core.getValue(m, n) * thisZeroed.getValue(i - halfCoreSize, j - halfCoreSize);
+                        }
+                    }
+                    result.setValue(i - halfCoreSize, j - halfCoreSize, _result);
+                }
+            }
+
+            console.log(result);
 
 
 
